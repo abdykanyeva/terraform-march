42e32304d56079fb1ce00d6c86166bb32f8b58f5
abdykanyeva <adelina.abdykanyeva@gmail.com>
2025-03-19 12:42:14 +0000

    added class1 files

---
+++ Class1/main.tf
@@ -0,0 +1,29 @@
+resource "aws_iam_user" "lb" {
+  name = "loadbalancer"
+
+}
+
+
+resource "aws_iam_user" "lb2" {
+  name = "kaizen"
+
+}
+
+resource "aws_iam_group" "developers" {
+  name = "developers"
+
+}
+
+
+resource "aws_iam_group_membership" "team" {
+  name = "tf-testing-group-membership"
+
+  users = [
+    aws_iam_user.lb2.name,
+    aws_iam_user.lb.name,
+  ]
+
+  group = aws_iam_group.developers.name
+}
+
+
